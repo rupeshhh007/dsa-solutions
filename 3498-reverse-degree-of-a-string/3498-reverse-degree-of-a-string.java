f8ce7747ecdc60a4a316715a0f1edd1eaa19fa35
Rupesh Tripathi <rupeshstripathi1234@gmail.com>
2026-09-20 20:00:18 +0530

    Time: 1 ms (100%), Space: 44.1 MB (50.47%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -3,12 +3,9 @@ class Solution {
         int res = 0;
         for(int i = 0; i<s.length();i++){
             char ch = s.charAt(i);
-            System.out.print(ch+", ");
             int val = 26-(ch-97);
             val*=i+1;
-            System.out.println(val+" ");
             res = res + val;
-
         }
         return res;
         
