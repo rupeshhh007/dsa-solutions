3b8995768f51b4b8d727f1424a6bb7110778484b
Rupesh Tripathi <rupeshstripathi1234@gmail.com>
2026-09-21 06:10:08 +0530

    Time: 4 ms (59.98%), Space: 50.7 MB (10.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,20 +3,26 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int dp[][] = new int[m+1][n+1];
+        int[] dp = new int[n];
 
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(i==0 && j==0)dp[i][j] = grid[i][j];
-                else if(i==0) dp[i][j] = grid[i][j] + dp[i][j-1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-                else if(j==0) dp[i][j] = grid[i][j] + dp[i-1][j];
-
-                else{
-                    dp[i][j]= grid[i][j] + Math.min( dp[i][j-1] , dp[i-1][j] );
+                if (i == 0 && j == 0) {
+                    dp[j] = grid[i][j];
+                } 
+                else if (i == 0) {
+                    dp[j] = grid[i][j] + dp[j - 1];
+                } 
+                else if (j == 0) {
+                    dp[j] = grid[i][j] + dp[j];
+                } 
+                else {
+                    dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
                 }
             }
         }
-        return dp[m-1][n-1];
+
+        return dp[n - 1];
     }
 }
